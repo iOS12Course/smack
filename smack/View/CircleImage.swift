70e1b7bffc651dee6174bf69286e892f843f9b0a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-22 22:13:18 -0500

    Generate Avatar BGColor and complete Create Account VC

---
+++ smack/View/CircleImage.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImage.swift
+//  smack
+//
+//  Created by Ricardo Herrera Petit on 1/22/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+}
