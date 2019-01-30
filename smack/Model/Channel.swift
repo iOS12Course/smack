a15d088a2acb79113fa98829fd9420911b423f4a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-29 20:18:49 -0500

    Channels service

---
+++ smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  smack
+//
+//  Created by Ricardo Herrera Petit on 1/29/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private (set) var channelTitle: String!
+    public private (set) var channelDescription: String!
+    public private (set) var id: String!
+    
+}
