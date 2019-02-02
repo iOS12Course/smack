acec32ab89ccf1e9a5ad0e9d5f0c0ed9bac1b17e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-02 18:48:17 -0500

    Displaying chat messages

---
+++ smack/View/MessageCell.swift
@@ -0,0 +1,32 @@
+//
+//  MessageCell.swift
+//  smack
+//
+//  Created by Ricardo Herrera Petit on 2/2/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    //MARK: - Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+        
+    }
+    
+    func configureCell(message:Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
