09543e12a873520fbe267e22daf87754602bd24b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-03 13:37:09 -0500

    unread channels

--- smack/Controller/ChatVC.swift
+++ smack/Controller/ChatVC.swift
@@ -41,8 +41,10 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNEL_SELECTED, object: nil)
         
-        SocketService.instance.getChatMassage { (success) in
-            if success {
+        SocketService.instance.getChatMassage { (newMessage) in
+            if newMessage.channelId == MessageService.instance.selectedChannel?.id  && AuthService.instance.isLoggedIn{
+                
+                MessageService.instance.messages.append(newMessage)
                 self.tableView.reloadData()
                 if MessageService.instance.messages.count > 0 {
                     let endIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
@@ -50,6 +52,15 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 }
             }
         }
+//        SocketService.instance.getChatMassage { (success) in
+//            if success {
+//                self.tableView.reloadData()
+//                if MessageService.instance.messages.count > 0 {
+//                    let endIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
+//                    self.tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+//                }
+//            }
+//        }
         
         SocketService.instance.getTypingUsers { (typingUsers) in
             guard let channelId = MessageService.instance.selectedChannel?.id else { return }
